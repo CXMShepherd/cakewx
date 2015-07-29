6db2e8f2afd9971a4d7cffae44c5ef5bc8d02926
niancode <niancode@gmail.com>
2015-07-30 03:29:52 +0800

    debug

--- app/Controller/MobController.php
+++ app/Controller/MobController.php
@@ -96,7 +96,13 @@ exit;
 		if ($wxCode) {
 			// $opens = $this->WxReply->getWpUserInfo('openid', $wxCode, $this->webchat, $this->appid);
 			$opens = file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid=".'wx708acc93bd6ad1b0'."&secret=".'d05a63f65046e95d4d41dc7858aa9624'."&code=".$wxCode."&grant_type=authorization_code");
+			$opens = json_decode($opens);
 			$this->log(var_export($opens, true)."\n", 'wxapi');exit;
+
+			$token = file_get_contents("https://api.weixin.qq.com/sns/userinfo?access_token={$opens['access_token']}&openid={$opens['openid']}&lang=zh_CN");
+			$this->log(var_export($token, true)."\n", 'wxapi');exit;
+
+
 			if ($opens['state'] == 1) {
 				$opens = $this->WxReply->getWpUserInfo('openid', $wxCode, $this->webchat, $this->appid);
 				$openid = $opens['data']['openid'];
