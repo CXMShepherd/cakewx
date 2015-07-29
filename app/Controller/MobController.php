59dd9e9018847c751c0f7b5cf57463977a4d1872
niancode <niancode@gmail.com>
2015-07-29 15:53:36 +0800

    Do test2

--- app/Controller/MobController.php
+++ app/Controller/MobController.php
@@ -125,7 +125,7 @@ class MobController extends AppController {
 			}
 		} else {
 			$rduri = urlencode(Router::url("/{$this->request->url}", TRUE));
-			$url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid={$this->appid}&redirect_uri={$rduri}&response_type=code&scope=snsapi_base&state=idaiyan#wechat_redirect";
+			$url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid={$this->appid}&redirect_uri={$rduri}&response_type=code&scope=snsapi_userinfo&state=idaiyan#wechat_redirect";
 			$this->redirect($url);
 		}
 	}
